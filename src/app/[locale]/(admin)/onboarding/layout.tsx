800a9a6d3a19e3732f76cf2cf19d94bbd23309ff
Mohammad RezaAli <158332844+MohammadMahdiRezaAli@users.noreply.github.com>
2024-09-22 10:05:59 +0330

    skip onboarding

--- src/app/[locale]/(admin)/onboarding/layout.tsx
+++ src/app/[locale]/(admin)/onboarding/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   // Check if a user has completed onboarding
-  // If yes, redirect them to /home
+  // If yes, redirect them to /dashboard
   if (auth().sessionClaims?.metadata?.onboardingComplete === true) {
     redirect('/home')
   }
