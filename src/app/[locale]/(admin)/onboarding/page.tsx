edaa54c97ddd8e253a71df100ae41dbb757520d0
Mohammad RezaAli <158332844+MohammadMahdiRezaAli@users.noreply.github.com>
2024-09-22 10:10:17 +0330

    fix the onboarding issue

--- src/app/[locale]/(admin)/onboarding/page.tsx
+++ src/app/[locale]/(admin)/onboarding/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation"; // updated for Next.js 13
 
 export default function Example() {
   const router = useRouter();
 
   useEffect(() => {
-    // Automatically redirect to home
+    // Ensure the code runs on the client-side and redirects
     router.push("/home");
-  }, []);
+  }, [router]);
 
   return (
     <div className="flex justify-center items-center h-screen">
