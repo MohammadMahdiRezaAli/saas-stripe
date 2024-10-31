61da2596200c401b50120845eb5ef497de44c1b9
Mohammad RezaAli <158332844+MohammadMahdiRezaAli@users.noreply.github.com>
2024-10-31 19:13:31 +0330

    hero section

--- src/components/ui/commons/HeroLanding.tsx
+++ src/components/ui/commons/HeroLanding.tsx
@@ -6,7 +6,12 @@ const HeroLanding = () => {
   const t = useTranslations("Index");
   return (
     <>
-      <div className="isolate pt-14" style={{ backgroundColor: '#1B1340' }}>
+      <div
+        className="isolate pt-14"
+        style={{
+          background: "linear-gradient(to right, #442EA1, #1B1340)",
+        }}
+      >
         {/* Animated Background */}
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 animate-gradient"
@@ -14,7 +19,7 @@ const HeroLanding = () => {
         >
           <div
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] 
-                       bg-gradient-to-tr from-[#462FA4] to-[#1B1340] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                       bg-gradient-to-tr from-[#442EA1] to-[#1B1340] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
